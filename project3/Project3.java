4d4397fe28a037f17b15d3c335347f484dec44f0
taica00 <taichenan00@gmail.com>
2022-11-02 19:08:55 +0800

    simple

---
+++ project3/Project3.java
@@ -0,0 +1,24 @@
+package project3;
+
+public class Project3 {
+    public static void main(String[] args) {
+        int[] w1 = new int[]{4, 6, 8};
+        int[] w2 = new int[]{5, 6, 8};
+        int[] p = new int[]{7, 6, 9};
+        System.out.println(knapsack(w1, p, 14));
+        System.out.println(knapsack(w2, p, 14));
+    }
+    
+    private static int knapsack(int[] w, int[] p, int C) {
+        int n = w.length;
+        int[] profit = new int[C+1];
+        for (int c = 1; c <= C; c++) {
+            profit[c] = profit[c-1];
+            for (int i = 0; i < n; i++) {
+                if (w[i] <= c)
+                    profit[c] = Math.max(profit[c], profit[c-w[i]] + p[i]);
+            }
+        }
+        return profit[C];
+    }
+}
